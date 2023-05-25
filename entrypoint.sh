5dcef6ae2d5e64ca014e3cd8e329112dacc0eed1
Evan Rosenfeld <evan.m.rosenfeld@gmail.com>
2023-05-25 18:46:52 -0400

    Fix for RPC failed; HTTP 408 curl 18 HTTP

--- entrypoint.sh
+++ entrypoint.sh
@@ -71,4 +71,6 @@ fi
 
 remote="https://$INPUT_ACTOR:$INPUT_TOKEN@github.com/$OUTPUT_REPOSITORY.git"
 
+git config --global http.version HTTP/1.1
+git config --global http.postBuffer 157286400
 git push $remote "HEAD:$INPUT_BRANCH" $force_option && exit 0
